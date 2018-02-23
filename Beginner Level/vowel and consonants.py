eeeb016fcd08252bc2c2fb9b6c473c913d5c623a
SreelakshmiPT <36722729+SreelakshmiPT@users.noreply.github.com>
2018-02-23 09:03:24 +0530

    Create vowel and consonants.py

---
+++ Beginner Level/vowel and consonants.py
@@ -0,0 +1,14 @@
+def main():
+ a=str(input())
+ if((a=='a')or(a=='e')or(a=='i')or(a=='o')or(a=='u')):
+    print("vowel")
+ else:
+    print("consonant")
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
